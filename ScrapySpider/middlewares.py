e9b6306f17fe8fd5fafa2d546fcaaef3968abf05
Reizk <543230868@qq.com>
2019-07-28 14:58:30 +0800

    Update middlewares.py

--- ScrapySpider/middlewares.py
+++ ScrapySpider/middlewares.py
@@ -15,8 +15,8 @@ class ProxyMiddleware(object):
     # overwrite process request
     def process_request(self, request, spider):
         # Set the location of the proxy
-        request.meta['proxy'] = "http://localhost:1080"
-
+        #request.meta['proxy'] = "http://localhost:1080"
+        pass
         # Use the following lines if your proxy requires authentication
         #proxy_user_pass = "USERNAME:PASSWORD"
         # setup basic authentication for the proxy
